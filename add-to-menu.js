ddfc7fd80b1da8c150b95d9b99c29c1811bd0a09
Paco Hurtado <francisco.hurtadof@gmail.com>
2021-06-03 10:45:16 -0500

    Update add-to-menu.js

--- add-to-menu.js
+++ add-to-menu.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function() {
 var listDiv =document.getElementsByClassName('menu__item_1eM w-nav__item menu__item--submenu_2Ot w-nav__item--submenu')[7]
+var listDiv2 =document.getElementsByClassName('menu__item_1eM w-nav__item menu__item--submenu_2Ot w-nav__item--submenu')[1]
 var div = document.createElement('div')
 div.innerHTML = document.getElementById('blockOfStuff').innerHTML
 listDiv.appendChild(div)
+listDiv2.appendChild(div)
 })
