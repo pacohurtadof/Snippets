7e917b061aa0e48265518d63c4791d65937bda42
Paco Hurtado <francisco.hurtadof@gmail.com>
2021-06-03 10:51:04 -0500

    Update add-to-menu.js

--- add-to-menu.js
+++ add-to-menu.js
@@ -3,7 +3,7 @@ var listDiv =document.getElementsByClassName('menu__item_1eM w-nav__item menu__i
 var listDiv2 =document.getElementsByClassName('menu__item_1eM w-nav__item menu__item--submenu_2Ot w-nav__item--submenu')[1]
 var div = document.createElement('div')
 div.innerHTML = document.getElementById('blockOfStuff').innerHTML
-var div2 = div.clone(true)
+var div2 = div.cloneNode(true)
 listDiv.appendChild(div)
 listDiv2.appendChild(div2)
 })
