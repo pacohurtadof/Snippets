8cc845db8b66b3df0f2810735cdc9c0ea75a0a79
Paco Hurtado <francisco.hurtadof@gmail.com>
2021-09-09 00:05:25 -0500

    Update kayal-menus-copy.js

--- kayal-menus-copy.js
+++ kayal-menus-copy.js
@@ -36,7 +36,7 @@ if(newElements.length>0){
 			subElements.style.overflow = "visible"
 			subElements.style.margin = "0"
 			subElements.style.marginBottom = "5px"
-			subElements.style.background = "none"
+			subElements.setAttribute("onmouseover", "this.style.backgroundColor='black'")
 			let temp= imageElement.cloneNode(true)
 			temp.style.display= "inline-block"
 			temp.style.height= "32px"
