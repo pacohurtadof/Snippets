fa62705609ed4b2643b0f8de3d3db5a1f05f1674
Paco Hurtado <francisco.hurtadof@gmail.com>
2021-09-08 23:35:51 -0500

    Update kayal-menus-copy.js

--- kayal-menus-copy.js
+++ kayal-menus-copy.js
@@ -1,22 +1,23 @@
-var specialtiesImages={
-	'Shoulder and Elbow Center':'url("https://repuso.com/assets/5295d87e2b054627535c8aefd86cc91d.png")',
-	'Hand and Wrist Center':'url("https://repuso.com/assets/5295d87e2b054627535c8aefd86cc91d.png")',
-	'Foot and Ankle Center':'',
-	'Podiatry Center':'',
-	'Hip & Knee Center':'',
-	'Hip Preservation Center':'',
-	'Rheumatology Center':'',
-	'Osteoporosis Center':'',
-	'Pain and Spine Center':'',
-	'Osteoarthritis and Orthobiologics Center':'url("https://repuso.com/assets/5295d87e2b054627535c8aefd86cc91d.png")',
-	'Sports Medicine':'',
-	'Chiropractic':'',
-	'Physical Therapy':'',
-	'Robotic Joint Replacement Center':'',
-	'Medical Imaging':'',
-	'Laser Treatment Center':'',
+<script> var specialtiesImages={
+	'Shoulder and Elbow Center':'url("https://i.imgur.com/0iH5PR2.png")',
+	'Hand and Wrist Center':'url("https://i.imgur.com/xL6s4JR.png")',
+	'Foot and Ankle Center':'url("https://i.imgur.com/aVLhrSQ.png")',
+	'Podiatry Center':'url("https://i.imgur.com/4JCoPJa.png")',
+	'Hip & Knee Center':'url("https://i.imgur.com/v11BwZp.png")',
+	'Hip Preservation Center':'url("https://i.imgur.com/QT0aa8M.png")',
+	'Rheumatology Center':'url("https://i.imgur.com/yi7KILZ.png")',
+	'Osteoporosis Center':'url("https://i.imgur.com/cFLp3ff.png")',
+	'Pain and Spine Center':'url("https://i.imgur.com/PNLSAR1.jpg")',
+	'Osteoarthritis and Orthobiologics':'url("https://i.imgur.com/IryicjL.png")',
+	'Sports Medicine':'url("https://i.imgur.com/B7wedxR.png")',
+	'Chiropractic':'url("https://i.imgur.com/SM7o2zP.png")',
+	'Physical Therapy':'url("https://i.imgur.com/mvLRqNN.png")',
+	'Robotic Joint Replacement':'url("https://i.imgur.com/jvLKwQe.png")',
+	'Medical Imaging':'url("https://i.imgur.com/SXJl4Qf.jpg")',
+	'Laser Treatment Center':'url("https://i.imgur.com/CHXSVbz.png")',
+    'Massage':'url("https://i.imgur.com/YcgmL0p.png?1")',
+    'Acupuncture':'url("https://i.imgur.com/niODa13.png?1")',
 }
-
 
 let searchedElement=document.getElementsByClassName("menu__item_1eM menu__item--light_UNr w-nav__item menu__item--submenu_2Ot w-nav__item--submenu")
 let newElements=[]
@@ -24,12 +25,16 @@ let imageElement= document.createElement('div')
 for(const elements of searchedElement){
 	if(elements.innerText.indexOf("Kayal Specialties Center")!==-1 && elements.parentElement.parentElement.parentElement.className.indexOf("header__burger_3P8") !== 0){
 		newElements.push(elements.getElementsByTagName("div")[0].children[0].children)
+		elements.getElementsByTagName("div")[0].children[0].style.overflow = "hidden"
 	}
 }
 if(newElements.length>0){
 	for(const elements of newElements){
 		for(const subElements of elements){
             		subElements.style.width = "100%"
+			subElements.style.overflow = "visible"
+			subElements.style.margin = "0"
+			subElements.style.marginBottom = "5px"
 			let temp= imageElement.cloneNode(true)
 			temp.style.display= "inline-block"
 			temp.style.height= "32px"
@@ -44,8 +49,9 @@ if(newElements.length>0){
 			tempSpan.style.display= "inline-block"
 			tempSpan.style.bottom= "11px"
 
-	}
+	    }
       
-		}
+	}
 }
-newElements=null
+//newElements=null
+  </script>
